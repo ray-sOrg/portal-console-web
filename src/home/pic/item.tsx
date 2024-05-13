e891d17bf58670cbe6d07c2e21c19a0f7305e97f
tangtao <tangtao@ascs.tech>
2024-05-13 09:48:10 +0800

    fix: update pic width height

--- src/home/pic/item.tsx
+++ src/home/pic/item.tsx
@@ -1,6 +1,14 @@
+import { MovieScreenWidth, MovieScreenHeight } from "../../utils";
+
 function PicItem({ src }: { src: string }) {
   return (
-    <div style={{ width: "80vw", height: "43.24vw", margin: "48px 0" }}>
+    <div
+      style={{
+        width: MovieScreenWidth,
+        height: MovieScreenHeight,
+        margin: "48px 0"
+      }}
+    >
       <img
         width="100%"
         height="100%"
---
+++ src/utils.ts
@@ -0,0 +1,4 @@
+const MovieScreenSize = 80;
+const MovieScreenRatio = 2.35 / 1;
+export const MovieScreenWidth = `${MovieScreenSize}vw`;
+export const MovieScreenHeight = `${MovieScreenSize / MovieScreenRatio}vw`;
