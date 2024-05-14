2511f938cf41144610da10ca89d24c95866fe1ad
tangtao <tangtao@ascs.tech>
2024-05-14 17:33:12 +0800

    init

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,7 +1,21 @@
+import { Layout } from "antd";
 import { Outlet } from "react-router-dom";
 
+const { Header, Sider, Content, Footer } = Layout;
+
 function Home() {
-  return <Outlet />;
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Header>Header</Header>
+      <Layout>
+        <Sider width="25%">Sider</Sider>
+        <Content>
+          <Outlet />
+        </Content>
+      </Layout>
+      <Footer>Footer</Footer>
+    </Layout>
+  );
 }
 
 export default Home;
